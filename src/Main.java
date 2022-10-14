59651d9da6b5e2997fd51eee1b99007cc308ac39
Kanyaton Somjit <sita@MacBook-Pro-som-tillhor-Kanyaton.local>
2022-10-14 14:27:27 +0200

    Level up

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,22 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
 
-        Map<String,String>  dictionary = new HashMap<>();
+        Map<String,String[]>  dictionary = new HashMap<>();
 
 
-        dictionary.put("hej","hello");
-        dictionary.put("middag","dinner");
-        dictionary.put(" fantastiskt","fantastic");
+        dictionary.put("hej",new String[] {"hey"," hello", " hi"});
+        dictionary.put("middag",new String[]{"dinner", " supper"});
+        dictionary.put(" fantastiskt",new String[]{"fantastic", " great"});
 
-        System.out.println(dictionary.get("hej"));
+        System.out.println(Arrays.toString(dictionary.get("hej")));
 
         for (String key : dictionary.keySet()){
-            System.out.println("Swedish :" + key);
-            System.out.println("English: " + dictionary.get(key));
+            System.out.println("\nSwedish :" + key);
+            System.out.println("English: " + Arrays.toString(dictionary.get(key)));
         }
     }
 }
